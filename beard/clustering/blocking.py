d9cf2ad2061ac8a9310cc9443075d08dd6c049b6
Grzegorz Jacenków <grzegorz.jacenkow@cern.ch>
2016-01-20 16:16:53 +0100

    blocking: multiprocessing
    * Removes multiprocessing for n_jobs = 1.
    
    Signed-off-by: Grzegorz Jacenków <grzegorz.jacenkow@cern.ch>

--- beard/clustering/blocking.py
+++ beard/clustering/blocking.py
@@ -16,7 +16,6 @@
 
 from __future__ import print_function
 
-import multiprocessing as mp
 import numpy as np
 import time
 
@@ -26,11 +25,6 @@ from sklearn.base import ClusterMixin
 from sklearn.utils import column_or_1d
 
 from .blocking_funcs import block_single
-
-try:
-    from multiprocessing import SimpleQueue
-except ImportError:
-    from multiprocessing.queues import SimpleQueue
 
 
 class _SingleClustering(BaseEstimator, ClusterMixin):
@@ -86,6 +80,35 @@ def _parallel_fit(fit_, partial_fit_, estimator, verbose, data_queue,
 
     data_queue.put(('end', None, None))
     return
+
+
+def _single_fit(fit_, partial_fit_, estimator, verbose, data):
+    """Run clusterer's fit function."""
+    block, existing_clusterer = data
+    b, X, y = block
+
+    if len(X) == 1:
+        clusterer = _SingleClustering()
+    elif existing_clusterer and partial_fit_ and not fit_:
+        clusterer = existing_clusterer
+    else:
+        clusterer = clone(estimator)
+
+    if verbose > 1:
+        print("Clustering %d samples on block '%s'..." % (len(X), b))
+
+    if fit_ or not hasattr(clusterer, "partial_fit"):
+        try:
+            clusterer.fit(X, y=y)
+        except TypeError:
+            clusterer.fit(X)
+    elif partial_fit_:
+        try:
+            clusterer.partial_fit(X, y=y)
+        except TypeError:
+            clusterer.partial_fit(X)
+
+    return (b, clusterer)
 
 
 class BlockClustering(BaseEstimator, ClusterMixin):
@@ -191,51 +214,83 @@ class BlockClustering(BaseEstimator, ClusterMixin):
         """Fit base clustering estimators on X."""
         self.blocks_ = blocks
 
-        processes = []
-        # Here the blocks will be passed to subprocesses
-        data_queue = SimpleQueue()
-        # Here the results will be passed back
-        result_queue = SimpleQueue()
-        for x in range(self.n_jobs):
-            processes.append(mp.Process(target=_parallel_fit, args=(self.fit_,
-                             self.partial_fit_, self.base_estimator,
-                             self.verbose, data_queue, result_queue)))
-            processes[-1].start()
+        if self.n_jobs == 1:
+            blocks_computed = 0
+            blocks_all = len(np.unique(blocks))
 
-        # First n_jobs blocks are sent into the queue without waiting for the
-        # results. This variable is a counter that takes care of this.
-        presend = 0
-        blocks_computed = 0
-        blocks_all = len(np.unique(blocks))
+            for block in self._blocks(X, y, blocks):
+                if self.partial_fit_ and block[0] in self.clusterers_:
+                    data = (block, self.clusterers_[block[0]])
+                else:
+                    data = (block, None)
 
-        for block in self._blocks(X, y, blocks):
-            if presend >= self.n_jobs:
-                b, clusterer = result_queue.get()
-                blocks_computed += 1
-                if clusterer:
-                    self.clusterers_[b] = clusterer
-            else:
-                presend += 1
-            if self.partial_fit_:
-                if block[0] in self.clusterers_:
-                    data_queue.put(('middle', block, self.clusterers_[b]))
-                    continue
+                b, clusterer = _single_fit(self.fit_, self.partial_fit_,
+                                           self.base_estimator, self.verbose,
+                                           data)
 
-            data_queue.put(('middle', block, None))
-
-        # Get the last results and tell the subprocesses to finish
-        for x in range(self.n_jobs):
-            if blocks_computed < blocks_all:
-                print("%s blocks computed out of %s" % (blocks_computed,
-                                                        blocks_all))
-                b, clusterer = result_queue.get()
-                blocks_computed += 1
                 if clusterer:
                     self.clusterers_[b] = clusterer
 
-        data_queue.put(('end', None, None))
+                if blocks_computed < blocks_all:
+                    print("%s blocks computed out of %s" % (blocks_computed,
+                                                            blocks_all))
+                blocks_computed += 1
+        else:
+            try:
+                from multiprocessing import SimpleQueue
+            except ImportError:
+                from multiprocessing.queues import SimpleQueue
 
-        time.sleep(1)
+            # Here the blocks will be passed to subprocesses
+            data_queue = SimpleQueue()
+            # Here the results will be passed back
+            result_queue = SimpleQueue()
+
+            for x in range(self.n_jobs):
+                import multiprocessing as mp
+                processes = []
+
+                processes.append(mp.Process(target=_parallel_fit, args=(
+                                 self.fit_, self.partial_fit_,
+                                 self.base_estimator, self.verbose,
+                                 data_queue, result_queue)))
+                processes[-1].start()
+
+            # First n_jobs blocks are sent into the queue without waiting
+            # for the results. This variable is a counter that takes care of
+            # this.
+            presend = 0
+            blocks_computed = 0
+            blocks_all = len(np.unique(blocks))
+
+            for block in self._blocks(X, y, blocks):
+                if presend >= self.n_jobs:
+                    b, clusterer = result_queue.get()
+                    blocks_computed += 1
+                    if clusterer:
+                        self.clusterers_[b] = clusterer
+                else:
+                    presend += 1
+                if self.partial_fit_:
+                    if block[0] in self.clusterers_:
+                        data_queue.put(('middle', block, self.clusterers_[b]))
+                        continue
+
+                data_queue.put(('middle', block, None))
+
+            # Get the last results and tell the subprocesses to finish
+            for x in range(self.n_jobs):
+                if blocks_computed < blocks_all:
+                    print("%s blocks computed out of %s" % (blocks_computed,
+                                                            blocks_all))
+                    b, clusterer = result_queue.get()
+                    blocks_computed += 1
+                    if clusterer:
+                        self.clusterers_[b] = clusterer
+
+            data_queue.put(('end', None, None))
+
+            time.sleep(1)
 
         return self
 
